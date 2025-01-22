8f47b401e89144f10ed3465955775a4eeec77c7b
Kaleb Bishop <Kalebwbishop@gmail.com>
2025-01-21 22:29:45 -0500

    Update server.py

--- Qsine/backend-api/server.py
+++ Qsine/backend-api/server.py
@@ -2,6 +2,8 @@ from flask import Flask, request, jsonify
 from werkzeug.utils import secure_filename
 import os
 import json
+import requests
+import re
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = 'uploads/'
@@ -43,13 +45,13 @@ def upload_image():
 def get_barcode_product(barcode):
 
     if not barcode:
-        return jsonify({"error": "No barcode provided"}), 400
+        return jsonify({"message": "No barcode provided"}), 400
 
     if not barcode.isdigit():
-        return jsonify({"error": "Invalid barcode type"}), 400
+        return jsonify({"message": "Invalid barcode type"}), 400
 
     if len(barcode) != 12:
-        return jsonify({"error": "Invalid barcode length"}), 400
+        return jsonify({"message": "Invalid barcode length"}), 400
 
     with open('data.json') as f:
         data = json.load(f)
@@ -58,7 +60,50 @@ def get_barcode_product(barcode):
         if product:
             return jsonify(product)
         else:
-            return jsonify({"error": "Product not found"}), 404
+
+            def attempt_to_find_product(barcode):
+                fdc_id = None
+                response = requests.get(f'https://api1.myfooddata.com/autocomplete/{barcode}')
+                if response.status_code == 200:
+                    search_data = response.json()
+                    if 'hits' in search_data and len(search_data['hits']) > 0:
+                        fdc_id = search_data['hits'][0]['document']['fdc_id']
+
+                if fdc_id:
+                    headers = {
+                        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+                    }
+                    
+                    response = requests.get(f'https://tools.myfooddata.com/nutrition-facts/{fdc_id}/wt1/1', headers=headers)
+                    if response.status_code == 200:
+                        match_name = re.search(r'"name":"(.*?)"', response.text)
+                        match_company = re.search(r'"brand_owner":"(.*?)"', response.text)
+                        match_ingredients = re.search(r'"ingredients":"(.*?)"', response.text)
+                        if match_name and match_company and match_ingredients:
+                            product = {
+                                "name": match_name.group(1),
+                                "company": match_company.group(1),
+                                "ingredients": match_ingredients.group(1).lower().split(', ')
+                            }
+
+                            with open('data.json') as f:
+                                data = json.load(f)
+                                data[barcode] = product
+
+                            with open('data.json', 'w') as f:
+                                json.dump(data, f, indent=4)
+
+                            return product
+                    
+                    return None
+            
+            product = attempt_to_find_product(barcode)
+            print(product)
+
+            if product:
+                return jsonify(product)
+
+            return jsonify({"message": "Product not found"}), 404
 
 if __name__ == '__main__':
-    app.run(debug=True, host='192.168.5.69')
+    app.run(debug=True, host='192.168.7.110')
